9bfc4ac13c20912beb7987247e071a0c43d91a66
administrador <administrador@Ubuntu22.04>
2024-04-17 12:22:09 +0200

    Commit del ejercicio 6

--- Calculadora.java
+++ Calculadora.java
@@ -3,3 +3,5 @@
 //Este comentario esta relacionado con el ejercicio numero 3
 
 //Este es el cambio desde el repositorio ONLINE, ejercicio numero 5
+
+//Este es el cambio realizado desde el repositorio local, ejercicio numero 6
