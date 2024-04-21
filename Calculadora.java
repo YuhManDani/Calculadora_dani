a9b8a2476c952a076c58b48a0af4fd6352c20618
administrador <administrador@Ubuntu22.04>
2024-04-21 11:31:02 +0200

    Conflicto main

--- Calculadora.java
+++ Calculadora.java
@@ -5,3 +5,5 @@
 //Este es el cambio desde el repositorio ONLINE, ejercicio numero 5
 
 //Este es el cambio realizado desde el repositorio local, ejercicio numero 6
+
+//Cambio  para conflicto realizado en: Main
