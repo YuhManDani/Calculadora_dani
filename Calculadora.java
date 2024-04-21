efda34c257eb7f3b741c80c8bcc080275c81aeb0
administrador <administrador@Ubuntu22.04>
2024-04-21 11:01:31 +0200

    Cambio desde la rama2

--- Calculadora.java
+++ Calculadora.java
@@ -5,3 +5,5 @@
 //Este es el cambio desde el repositorio ONLINE, ejercicio numero 5
 
 //Este es el cambio realizado desde el repositorio local, ejercicio numero 6
+
+//Este cambio ha sido realizado en la rama2.
